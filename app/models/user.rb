1417c24726fa8b4a2f360fee665bb31e5aa9b557
takumi.nakashima <nakashima.2004.abenomix@gmail.com>
2021-01-09 07:09:57 +0000

    add image カラムにメソッドを追加 mount_uploaderメソッドでimageカラムに追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,5 @@ class User < ApplicationRecord
   validates :password, presence: true, length: { minimum: 8 },
                     format: { with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{7,}/ }
   has_secure_password
+  mount_uploader :image, ImageUploader
 end
