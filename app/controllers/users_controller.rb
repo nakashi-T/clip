560f4c580a3e5e366fca076da6dd36b238eb982b
takumi.nakashima <nakashima.2004.abenomix@gmail.com>
2021-01-09 05:13:02 +0000

    add 会員登録のcreateアクションを追加 併せて新規ユーザーのStrong Paramterを追加している。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,19 +22,25 @@ class UsersController < ApplicationController
 
   # POST /users
   # POST /users.json
-  # def create
-  #   @user = User.new(user_params)
+  def create
+    @user = User.new(user_params)
 
-  #   respond_to do |format|
-  #     if @user.save
-  #       format.html { redirect_to @user, notice: 'User was successfully created.' }
-  #       format.json { render :show, status: :created, location: @user }
-  #     else
-  #       format.html { render :new }
-  #       format.json { render json: @user.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
+    if @user.save
+      redirect_to @user
+    else
+      render :new
+    end
+    
+    # respond_to do |format|
+    #   if @user.save
+    #     format.html { redirect_to @user, notice: 'User was successfully created.' }
+    #     format.json { render :show, status: :created, location: @user }
+    #   else
+    #     format.html { render :new }
+    #     format.json { render json: @user.errors, status: :unprocessable_entity }
+    #   end
+    # end
+  end
 
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
@@ -61,6 +67,10 @@ class UsersController < ApplicationController
   end
 
   private
+    
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
     # Use callbacks to share common setup or constraints between actions.
     # def set_user
     #   @user = User.find(params[:id])
