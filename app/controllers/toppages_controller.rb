2b9ee2c5bb59ae62d105ec53585383afe5ac40ec
takumi.nakashima <nakashima.2004.abenomix@gmail.com>
2021-01-10 07:08:03 +0000

    update 投稿一覧のページネーションを30件から20件に変更

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,5 +1,5 @@
 class ToppagesController < ApplicationController
   def index
-    @posts = Post.order(id: :desc).page(params[:page]).per(30)
+    @posts = Post.order(id: :desc).page(params[:page]).per(20)
   end
 end
