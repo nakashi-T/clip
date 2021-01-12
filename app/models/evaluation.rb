7b763c5df7408cf4aeb920771cc3da8e3a73d741
takumi.nakashima <nakashima.2004.abenomix@gmail.com>
2021-01-12 10:10:54 +0000

    add 星評価するための設定記述 しかしまだrailsのエラーはないがjQueryでエラーがあり、表示はできていない

--- app/models/evaluation.rb
+++ app/models/evaluation.rb
@@ -3,4 +3,10 @@ class Evaluation < ApplicationRecord
   
   validates :item_title_1, presence: true
   validates :item_amount_1, presence: true
+  
+  def change
+    create_table :posts do |t|
+      t.float :item_amount_1
+    end
+  end
 end
