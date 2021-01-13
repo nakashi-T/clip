f35fb0116e4078827a66e9e2387700fac400bff1
takumi.nakashima <nakashima.2004.abenomix@gmail.com>
2021-01-13 11:57:34 +0000

    upadate ユーザー一覧でユーザのフォロー数を表示 固定して表示する

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,5 +1,7 @@
 class ToppagesController < ApplicationController
   def index
     @posts = Post.order(id: :desc).page(params[:page]).per(20)
+    @followings = current_user.followings
   end
 end
+
