1d06cbb1d835906c7663ab3017206a229bd0e3a6
takumi.nakashima <nakashima.2004.abenomix@gmail.com>
2021-01-13 04:27:15 +0000

    add relationshipsのモデル設定 throughを使いどのカラムの情報を取得するか指定した。

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,9 @@ class User < ApplicationRecord
   mount_uploader :image, ImageUploader
   
   has_many :posts
+  
+  has_many :relationships
+  has_many :followings, through: :relationships, source: :follow
+  has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
+  has_many :followers, through: :reverses_of_relationship, source: :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_11_085106) do
+ActiveRecord::Schema.define(version: 2021_01_13_034923) do
 
   create_table "evaluations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "post_id"
@@ -34,6 +34,16 @@ ActiveRecord::Schema.define(version: 2021_01_11_085106) do
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
+  create_table "relationships", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "follow_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["follow_id"], name: "index_relationships_on_follow_id"
+    t.index ["user_id", "follow_id"], name: "index_relationships_on_user_id_and_follow_id", unique: true
+    t.index ["user_id"], name: "index_relationships_on_user_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -45,4 +55,6 @@ ActiveRecord::Schema.define(version: 2021_01_11_085106) do
 
   add_foreign_key "evaluations", "posts"
   add_foreign_key "posts", "users"
+  add_foreign_key "relationships", "users"
+  add_foreign_key "relationships", "users", column: "follow_id"
 end
