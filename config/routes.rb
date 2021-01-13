a7e0c73df69d0bf50be8d9be697d587453adc513
takumi.nakashima <nakashima.2004.abenomix@gmail.com>
2021-01-13 15:24:01 +0000

    add favoriteのルーティングを追加 create,destroyアクションとuserとpostのいいねページへのルーティングの作成

--- config/routes.rb
+++ config/routes.rb
@@ -12,14 +12,17 @@ Rails.application.routes.draw do
     member do
       get :followings
       get :followers
+      get 'likes', to: 'users#likes'
     end
   end
   
   resources :posts, only: [:new, :create, :show] do
     member do
       post :evaluation
+      get 'likes', to: 'posts#likes'
     end 
   end
   
   resources :relationships, only: [:create, :destroy]
+  resources :favorites, only: [:create, :destroy]
 end
