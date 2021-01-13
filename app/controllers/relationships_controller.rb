12a86a2d8fb9c4f661b16111828d6cdd31492715
takumi.nakashima <nakashima.2004.abenomix@gmail.com>
2021-01-13 08:15:42 +0000

    update ユーザー一覧にフォローボタンを追加 併せて各ページのボタンを修正

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -3,13 +3,14 @@ class RelationshipsController < ApplicationController
     user = User.find(params[:follow_id])
     current_user.follow(user)
     flash[:success] = 'フォローしました。'
-    redirect_to user
+    # redirect_to root_url
+    redirect_back(fallback_location: root_path)
   end
 
   def destroy
     user = User.find(params[:follow_id])
     current_user.unfollow(user)
     flash[:success] = 'フォローを解除しました。'
-    redirect_to user
+    redirect_back(fallback_location: root_path)
   end
 end
