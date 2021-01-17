2745731c0a03ce06278cc28e97880001b4ca8941
takumi.nakashima <nakashima.2004.abenomix@gmail.com>
2021-01-18 03:33:43 +0900

    update 自己評価の項目のバリデーションを追加 どちらかが入力されていないとエラーになる仕様

--- app/models/evaluation.rb
+++ app/models/evaluation.rb
@@ -4,8 +4,11 @@ class Evaluation < ApplicationRecord
   validates :item_title_1, presence: true
   validates :item_amount_1, presence: true
   
+  validates :item_title_2, presence: true, if: Proc.new { |evaluation| evaluation.item_amount_2.present? }
   validates :item_amount_2, presence: true, if: Proc.new { |evaluation| evaluation.item_title_2.present? }
   
+  validates :item_title_3, presence: true, if: Proc.new { |evaluation| evaluation.item_amount_3.present? }
+  validates :item_amount_3, presence: true, if: Proc.new { |evaluation| evaluation.item_title_3.present? }
   
   #小数点で入力したい時に行う
   # def change
