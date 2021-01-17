853b66e88d3e97eab5ff167861a11796415f20ac
takumi.nakashima <nakashima.2004.abenomix@gmail.com>
2021-01-17 16:38:20 +0900

    fix 新規投稿ページと検索ページのhtmlエラーを修正 新規投稿はlabel削除と検索はidを付与する修正を行なった

--- app/models/evaluation.rb
+++ app/models/evaluation.rb
@@ -4,6 +4,13 @@ class Evaluation < ApplicationRecord
   validates :item_title_1, presence: true
   validates :item_amount_1, presence: true
   
+  # validates :item_amount_2, presence: true
+  
+  # def item_allow
+  # allow_blank: true
+  # end
+  
+  
   #小数点で入力したい時に行う
   # def change
   #   create_table :posts do |t|
