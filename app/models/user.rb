31d4706d63baafb17883c875b622554d94069ea5
takumi.nakashima <nakashima.2004.abenomix@gmail.com>
2021-01-19 10:36:38 +0900

    add ユーザー削除機能を追加 外部キーのテーブルも削除するように設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,14 +10,14 @@ class User < ApplicationRecord
   has_secure_password
   mount_uploader :image, ImageUploader
   
-  has_many :posts
+  has_many :posts, dependent: :destroy
   
-  has_many :relationships
+  has_many :relationships, dependent: :destroy
   has_many :followings, through: :relationships, source: :follow
-  has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
+  has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id', dependent: :destroy
   has_many :followers, through: :reverses_of_relationship, source: :user
   
-  has_many :favorites
+  has_many :favorites, dependent: :destroy
   has_many :favoriteings, through: :favorites, source: :post
   
   
